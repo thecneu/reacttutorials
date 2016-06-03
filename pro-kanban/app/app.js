050fa6b1902621ed45bf88fdb19dcd06d7e30156
Corey Newhouse <corey.newhouse@deutschinc.com>
2016-06-03 09:31:24 -0700

    added comments

--- pro-kanban/app/app.js
+++ pro-kanban/app/app.js
@@ -2,6 +2,23 @@ import React from 'react';
 
 export default class App extends React.Component {
   render() {
+    /*
+    3 Lists
+      Header
+        Title
+        Add
+        Status #s
+
+      Lists
+        Card
+          Title
+          Description
+          TaskList
+            Checkbox
+            Label
+          Add Task
+
+    */
 
   }
 }
