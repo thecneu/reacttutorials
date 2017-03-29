006c0b75587d7b95af9b1c69032aa4e5076cabe6
corey.newhouse <Corey.Newhouse@deutschinc.com>
2017-03-29 11:23:30 -0700

    initial clear

--- do/src/components/ListItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ListItem = ({id, text, completed, todoClick}) => (
-    <li style={{textDecoration: completed ? 'line-through' : 'none'}} onClick={() => todoClick(id)}>
-        {text}
-    </li>
-);
-
-export default ListItem;
--- redux-gallery/src/components/Thumb.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Thumb = ({index, children, changeSelectedImage}) => (
-    <div>
-        <img onClick={changeSelectedImage} src={children} alt=""/>
-    </div>
-);
-
-export default Thumb;
